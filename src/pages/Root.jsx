7d2e4530a0072d05ac89f5297aa78dd87fad0139
Ben Sales <adebsa2401@gmail.com>
2022-12-08 12:55:00 +0100

    feat: Add styling to the header and the Categories page

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import Avatar from '../images/person-fill.svg';
+import '../styles/Root.css';
 
 export default class Root extends React.PureComponent {
   render() {
     return (
       <div>
         <header>
-          <h1>Bookstore CMS</h1>
+          <Link to="/" className="header-logo">Bookstore CMS</Link>
           <nav>
-            <Link to="/">Books</Link>
-            <Link to="/categories">Categories</Link>
+            <Link className="nav-link active" to="/">Books</Link>
+            <Link className="nav-link" to="/categories">Categories</Link>
           </nav>
-          <img src={Avatar} alt="avatar" />
+          <img className="profile-avatar" src={Avatar} alt="avatar" />
         </header>
         <main>
           <Outlet />
