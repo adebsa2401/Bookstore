3779e44bd2e0e218e75d864bc06360176c636aa6
Ben Sales <adebsa2401@gmail.com>
2022-12-16 22:30:26 +0100

    feat: connect status checking to redux store

--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 export default function CategoriesPage() {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories);
+
+  const handleCheckStatus = () => {
+    dispatch(checkStatus());
+  };
+
   return (
     <div>
-      <button type="button">Check status</button>
+      <button type="button" onClick={handleCheckStatus}>Check status</button>
+      <h1>{categories}</h1>
     </div>
   );
 }
